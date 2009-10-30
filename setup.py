51ac203918ba3e681cb55f8d29f83ffc07302a33
emergentmusics <emergentmusics@36f24fd6-c56f-11de-8df7-59987fa32379>
2009-10-30 17:26:29 +0000

    First Public Release.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='MIDIUtil',
-      version='HEAD',
+      version='0.87',
       description='MIDIUtil, a MIDI Interface for Python',
       author='Mark Conway Wirt',
       author_email='emergentmusics) at (gmail . com',
       license='Copyright (C) 2009, Mark Conway Wirt. See License.txt for details.',
-      url='www.emergentmusics.org',
+      url='http://www.emergentmusics.org/midiutil/',
       packages=["midiutil"],
       package_dir = {'midiutil': 'src/midiutil'},
       package_data={'midiutil' : ['../../documentation/*']},
